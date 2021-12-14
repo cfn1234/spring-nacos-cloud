398a302d93be0985e833762849c4a12e616ca65a
caofengnian <435450916@qq.com>
2021-12-14 16:43:41 +0800

    feat:提交代码

--- cloud-consumer/src/main/java/com/thunder/NacosConsumerApplication.java
+++ cloud-consumer/src/main/java/com/thunder/NacosConsumerApplication.java
@@ -1,13 +1,19 @@
 package com.thunder;
 
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author caofengnian
+ * EnableAutoDataSourceProxy后使用注解开启DataSourceProxy
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAutoDataSourceProxy
 public class NacosConsumerApplication {
 
 	public static void main(String[] args) {
